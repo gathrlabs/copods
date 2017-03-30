a9ff27c9d29e24651b117a5b2a27623c6680477a
Tom Scerri Jr <tomscerri@gathrlabs.com.au>
2017-03-30 13:33:18 +0000

    first commit

---
+++ src/components/registration/Registration.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>for routers test</p>
+    
+</template>
+
+
+<script>
+    export default {
+        name: 'registration'
+    }
+</script>
+
+<style lang="sass" scoped>
+@import '~bulma';
+    
+</style>
