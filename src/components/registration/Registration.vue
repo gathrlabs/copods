d50fd9bb55cdebbd6c1c0bd37b1acced1a3fd165
Tom Scerri Jr <tomscerri@gathrlabs.com.au>
2017-04-04 15:02:21 +0000

    Registration styling completed

--- src/components/registration/Registration.vue
+++ src/components/registration/Registration.vue
@@ -7,13 +7,108 @@
           <h1 class="title title-left is-1">This revolution - like any good revolution - is free to join.</h1>
           <div class="socials">
             <h3 class="title is-3 center">You can also sign up with one of your social media accounts.</h3>
+            <div class="columns is-gapless">
+              <div class="column is-half">
+                <a class="auth-Button google"><svg width="45" height="45"><path d="M25.062604,22.9606299 L25.062604,26.0220472 L30.4994652,26.0220472 C30.2342525,27.2976378 28.9081888,29.8488189 25.1952104,29.8488189 C22.0126575,29.8488189 19.4931365,27.2976378 19.4931365,24.2362205 C19.4931365,21.1748031 22.1452639,18.623622 25.1952104,18.623622 C27.0516996,18.623622 28.2451569,19.3889764 28.9081888,20.0267717 L31.4277098,17.7307087 C29.8364333,16.2 27.7147314,15.3070866 25.1952104,15.3070866 C20.0235619,15.3070866 15.9127645,19.2614173 15.9127645,24.2362205 C15.9127645,29.2110236 20.0235619,33.1653543 25.1952104,33.1653543 C30.4994652,33.1653543 34.0798372,29.5937008 34.0798372,24.4913386 C34.0798372,23.8535433 34.0798372,23.4708661 33.9472308,22.9606299 L25.062604,22.9606299 L25.062604,22.9606299 L25.062604,22.9606299 Z" id="Shape" stroke="none" fill-rule="evenodd"></path></svg>Google</a>
+              </div>
+              <div class="column is-half">                    
+                <a class="auth-Button facebook"><svg width="45" height="45"><path d="M22.5734896,34.4409449 L22.543083,25.511811 L18.5648919,25.511811 L18.5648919,21.6850394 L22.543083,21.6850394 L22.543083,19.1338583 C22.543083,15.6907843 24.7596117,14.0314961 27.9526007,14.0314961 C29.4820693,14.0314961 30.796583,14.141031 31.1796563,14.1899882 L31.1796563,17.7881995 L28.9651565,17.789169 C27.2286363,17.789169 26.8923995,18.5829307 26.8923995,19.7477235 L26.8923995,21.6850394 L31.8255289,21.6850394 L30.4994652,25.511811 L26.8923863,25.511811 L26.8923863,34.4409449 L22.5734896,34.4409449 L22.5734896,34.4409449 Z" id="Shape" stroke="none" fill-rule="evenodd"></path></svg>Facebook</a>
+              </div>
+            </div>
           </div>
         </article>
       </div>
       <div class="right-half">
+        <div class="close">
+          <a class="delete is-large"></a>
+        </div>
        <article>
-         <h1>Right Half</h1>
-         <p>If your knees aren't green by the end of the day, you ought to seriously re-examine your life.</p>
+         <h2 class="title is-2">Sign Up</h2>
+         <div class="form">
+           <h5 class="is-5">Enter your details to get started, or use your google or facebook account to sign-up.</h5>
+           <div class="columns">
+             <div class="column is-half">
+               <div class="field">
+                  <label class="label">First Name</label>
+                    <p class="control">
+                    <input class="input double" type="text" placeholder="First Name">
+                  </p>
+                </div>
+             </div>
+             <div class="column is-half">
+                <div class="field">
+                  <label class="label">Surname</label>
+                    <p class="control">
+                    <input class="input double" type="text" placeholder="Surname">
+                  </p>
+                </div>
+             </div>
+            </div>
+            <div class="columns">
+              <div class="column is-half">
+                <div class="field">
+                <label class="label">Email</label>
+                  <p class="control">
+                  <input class="input double" type="email" placeholder="Email">
+                  </p>
+                </div>
+              </div>
+              <div class="column is-half">
+                <div class="field">
+                <label class="label">Confirm Email</label>
+                  <p class="control">
+                  <input class="input double" type="email" placeholder="Confirm Email">
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="columns">
+             <div class="column is-half">
+               <div class="field">
+                  <label class="label">Password</label>
+                    <p class="control">
+                    <input class="input double" type="password" placeholder="Password">
+                  </p>
+                </div>
+             </div>
+             <div class="column is-half">
+                <div class="field">
+                  <label class="label">Confirm Password</label>
+                    <p class="control">
+                    <input class="input double" type="password" placeholder="Confirm Password">
+                  </p>
+                </div>
+             </div>
+            </div>
+            <div class="columns" style="margin-top: 30px">
+              <div class="column is-12">
+                <div class="field">
+                  <a class="std-button submit">Submit</a>
+                </div>
+              </div>
+            </div>
+            <div class="columns" style="margin-top: 10px">
+             <div class="column is-half">
+              <a href="#" class="text-button">Forgot Password
+                <svg width="17px" height="12px" viewBox="62 8 17 12">
+                  <path d="M74.6568542,14.6557061 L63.0008717,14.6557061 C62.4481055,14.6557061 62,14.2118413 62,13.6557061 C62,13.1034214 62.4446309,12.6557061 63.0008717,12.6557061 L74.6568542,12.6557061 L71.7050068,9.70385865 C71.3156423,9.31449416 71.3138598,8.68499205 71.7071068,8.2917451 C72.0976311,7.9012208 72.7345586,7.90498336 73.1192203,8.28964509 L77.366061,12.5364858 C77.4547709,12.6251956 77.5233623,12.7263705 77.5717472,12.8343763 C77.8306518,13.0143647 78,13.3138208 78,13.6557061 C78,13.9968594 77.8303426,14.2981123 77.569521,14.4785852 C77.521583,14.5865303 77.4537102,14.6872773 77.366061,14.7749265 L73.1192203,19.0217672 C72.7298558,19.4111317 72.1003537,19.4129141 71.7071068,19.0196672 C71.3165825,18.6291429 71.320345,17.9922153 71.7050068,17.6075536 L74.6568542,14.6557061 Z" class="arrow" stroke="none" fill-rule="evenodd"></path>
+                </svg>
+              </a> 
+             </div>
+             <div class="column is-half">
+              <a href="#" class="text-button">Login
+                <svg width="17px" height="12px" viewBox="62 8 17 12">
+                  <path d="M74.6568542,14.6557061 L63.0008717,14.6557061 C62.4481055,14.6557061 62,14.2118413 62,13.6557061 C62,13.1034214 62.4446309,12.6557061 63.0008717,12.6557061 L74.6568542,12.6557061 L71.7050068,9.70385865 C71.3156423,9.31449416 71.3138598,8.68499205 71.7071068,8.2917451 C72.0976311,7.9012208 72.7345586,7.90498336 73.1192203,8.28964509 L77.366061,12.5364858 C77.4547709,12.6251956 77.5233623,12.7263705 77.5717472,12.8343763 C77.8306518,13.0143647 78,13.3138208 78,13.6557061 C78,13.9968594 77.8303426,14.2981123 77.569521,14.4785852 C77.521583,14.5865303 77.4537102,14.6872773 77.366061,14.7749265 L73.1192203,19.0217672 C72.7298558,19.4111317 72.1003537,19.4129141 71.7071068,19.0196672 C71.3165825,18.6291429 71.320345,17.9922153 71.7050068,17.6075536 L74.6568542,14.6557061 Z" class="arrow" stroke="none" fill-rule="evenodd"></path>
+                </svg>
+              </a>
+             </div>
+            </div>
+            <div class="columns" style="margin-top: 30px">
+              <div class="column is-12">
+                <a class="text-button secondary"><i class="fa fa-external-link"></i>By continuing you are agreeing to our terms of service.</a>
+              </div>
+            </div>
+          </div>
         </article>
       </div>
      </section>
@@ -35,46 +130,282 @@
 @import "~bulma/sass/base/_all";
 @import "~bulma/sass/custom/mixins";
 @import "~bulma/sass/elements/_all";
-
-
 @import "~bulma/sass/custom/mixins";
-@import "~include-media/dist/_include-media"
+
+@import "~include-media/dist/_include-media";
+
+/*@import "~font-awesome/scss/font-awesome";*/
+
 
 html, body, section 
   height: 100%
+  background-color: #61d7a7;
+  background-image: url("data:image/svg+xml,%3Csvg width='80' height='40' viewBox='0 0 80 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23809cfc' fill-opacity='0.99' fill-rule='evenodd'%3E%3Cpath d='M2.01 39.976c.02-4.594 1.786-9.182 5.302-12.687.475-.475.97-.917 1.483-1.327v9.77L4.54 39.977H2.01zm5.374 0L23.842 23.57c.687 5.35-1.03 10.95-5.154 15.06-.483.482-.987.93-1.508 1.346H7.384zm-7.384 0c.018-5.107 1.982-10.208 5.89-14.104 5.263-5.247 12.718-6.978 19.428-5.192 1.783 6.658.07 14.053-5.137 19.296H0zm10.806-15.41c3.537-2.116 7.644-2.92 11.614-2.415L10.806 33.73v-9.164zM65.25.75C58.578-1.032 51.164.694 45.93 5.93c-5.236 5.234-6.962 12.648-5.18 19.32 6.672 1.782 14.086.056 19.32-5.18 5.236-5.234 6.962-12.648 5.18-19.32zM43.632 23.783c5.338.683 10.925-1.026 15.025-5.126 4.1-4.1 5.81-9.687 5.126-15.025l-20.15 20.15zm7.186-19.156c3.518-2.112 7.602-2.915 11.55-2.41l-11.55 11.55v-9.14zm-3.475 2.716c-4.1 4.1-5.81 9.687-5.126 15.025l6.6-6.6V6.02c-.51.41-1 .85-1.474 1.323zM.07 0C.066 1.766.29 3.533.75 5.25c6.672 1.782 14.086.056 19.32-5.18l.072-.07H.072zm17.087 0C13.25 3.125 8.345 4.386 3.632 3.783L7.414 0h9.743zM2.07 0c-.002.79.047 1.582.147 2.368L4.587 0H2.07z'/%3E%3C/g%3E%3C/svg%3E")
 
 div 
   display: flex
   flex-direction: column
   justify-content: center
   
+
+  
+.title 
+  color: white
+  text-shadow: 0 4px 6px rgba(0,0,0,0.50)
+  font-weight: 500
+  
+.title.title-left
+  text-align: left !important
+  
+.title.is-2 
+  color: #809CFC !important
+  font-weight: 700
+  text-shadow: 0 4px 6px rgba(0,0,0,0)
+  font-size: 3rem
+  
 .wrapper 
   height: 100vh
   width: 100vw
   display: flex
+  justify-content: space-around
 
 .left-half 
   background-color: #ff9e2c
   flex: 1
   padding: 6rem
-  @include media("<desktop")
+  @include media("<1200px")
     display: none
 
 .right-half 
   flex: 1
   padding: 1rem
+  /*when layout is double*/
+  @include media(">1200px")
+    background-color: white;
 
-.title 
-  color: white
-  text-shadow: 0 4px 6px rgba(0,0,0,0.50)
-  
-.title.title-left
-  text-align: left !important
+.close 
+  position: absolute
+  right: 25px
+  top: 25px
+
 
 .has-bg 
     @include cover-background('../../assets/img/registration-background-left.jpg');
     /*min-height: 100%;*/
 
+
+/*Socials*/
+
 .socials 
+  padding-left: 1rem
+  padding-right: 1rem
+  position: absolute
+  width: 30%
+  left: 10%
+  bottom: 3rem
+  @include media("<1650px")
+    width: 50%
+    left: 3% 
+
+/*Socials content*/
+
+.columns
+  flex-direction: row
   
+.column
+  justify-content: center
+  padding: 0
+  padding-top: 2rem
+  
+
+.auth-Button 
+    white-space: nowrap
+    display: inline-block
+    height: 50px
+    line-height: 50px
+    padding-right: 40px
+    box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)
+    background: #fff
+    border-radius: 4px
+    font-family: 'Lato', sans-serif
+    font-size: 17px
+    font-weight: 400
+    letter-spacing: .085em
+    color: #6772e5
+    text-decoration: none
+    transition: all .35s
+    cursor: pointer
+    width: 200px
+
+
+.auth-Button:hover 
+  box-shadow: 0 8px 7px 0 rgba(0,0,0,0.15) !important
+  transform: translateY(-1px)
+
+
+.auth-Button:active 
+  transform: translateY(1px)
+
+
+.auth-Button svg 
+    float: left
+    top: 1px
+    margin-right: 30px
+    fill: #FFF
+
+
+.auth-Button.google 
+    margin-right: 10px
+    color: #fff
+    background: #FF9393
+    text-shadow: 0 1px 3px rgba(36,180,126,.4)
+
+
+
+.auth-Button.google:hover 
+    background: #F46565
+    box-shadow: 0 6px 8px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)
+
+
+.auth-Button.google:active 
+    background: #DD3A3A
+
+
+.auth-Button.google:hover svg 
+    fill: #FFE
+
+.auth-Button.facebook 
+    margin-right: 5px
+    color: #fff
+    background: #4B74FF
+    text-shadow: 0 1px 3px rgba(36,180,126,.4)
+   
+
+
+.auth-Button.facebook:hover 
+    background: #1444E7
+    box-shadow: 0 6px 8px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)
+
+
+.auth-Button.facebook:active 
+    background: #0F37C1
+
+
+.auth-Button.facebook:hover svg 
+    fill: #FFE
+    
+/*Form content*/
+
+.form 
+  margin-left: 8rem
+  margin-right: 8rem
+  @include media("<1700px")
+    margin-left: 3rem;
+    margin-right: 3rem;
+  
+/*inputs*/
+
+input 
+  font-size: 13px
+  padding: 8px
+  width: 100%
+  
+input.single
+  width: 95%
+  
+input.double
+  width: 90%
+
+
+label 
+  font-family: 'Lato', sans-serif !important
+  font-weight: 400 !important
+  color: #6B7C93 !important
+  text-align: left
+
+
+input:focus 
+  border: 1px solid #4990E2 !important
+  box-shadow: 0 0 10px 0 #4A90E2
+
+
+input:hover 
+  border: 1px solid #4990E2 !important
+
+/*buttons*/
+
+.std-button 
+    white-space: nowrap
+    display: inline-block
+    height: 50px
+    line-height: 50px
+    padding-right: 40px
+    padding-left: 40px
+    box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)
+    background: #fff
+    border-radius: 4px
+    font-family: 'Lato', sans-serif
+    font-size: 17px
+    font-weight: 400
+    letter-spacing: .085em
+    color: #6772e5
+    text-decoration: none
+    transition: all .35s
+    cursor: pointer
+
+.submit
+    margin-left: 13rem
+    margin-right: 13rem
+    @include media(">1800px")
+      margin-left: 18rem
+      margin-right: 18rem
+      
+    
+
+.std-button:hover 
+  box-shadow: 0 8px 7px 0 rgba(0,0,0,0.15) !important
+  transform: translateY(-1px)
+
+
+.std-button:active 
+  transform: translateY(1px)
+
+
+.text-button 
+  /* View office: */
+  font-family: Avenir-Heavy
+  font-size: 1rem
+  color: #39CD90
+  letter-spacing: 0
+  line-height: 24px
+  transition: all 0.3s ease
+
+
+.text-button:hover 
+  color: #15c179
+
+
+.text-button svg 
+  fill: #39CD90
+  padding-left: 5px
+  transition: all 0.3s ease
+  
+
+
+.text-button:hover svg 
+  fill: #39CD90
+  padding-left: 8px
+  
+.secondary 
+  /* View office: */
+  font-size: .95 rem
+  color: #6B7C93
+  text-decoration: underline
+
+
+.secondary:hover 
+   color: #97a3b2
+
+
+    
 </style>
